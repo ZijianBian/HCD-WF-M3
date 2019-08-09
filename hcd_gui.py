2fb79b7294eb18c62259338f670b9695fc80ae94
Mireille Schneider <Mireille.Schneider@iter.org>
2019-08-09 14:10:56 +0200

    Corrected typo in core_sources for the list of actors in the GUI interface

--- hcd_gui.py
+++ hcd_gui.py
@@ -19,7 +19,7 @@ from developer_file import load_code_dependencies
 
 actor_path = os.path.join(os.getenv('KEPLER'), 'imas/src/org/iter/imas/python')
 
-ids_list = ['core_profiles','core_sourcres','equilibrium', 'pulse_schedule', 'nbi', 'ic_antennas', 'ec_antennas','wall', 'distribution_sources', 'distributions', 'waves']
+ids_list = ['core_profiles','core_sources','equilibrium', 'pulse_schedule', 'nbi', 'ic_antennas', 'ec_antennas','wall', 'distribution_sources', 'distributions', 'waves']
 
 def read_inputoutput(name):
 
