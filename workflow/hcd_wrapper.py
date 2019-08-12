f01e1828c8522c6fb9f70a7d5bcbf2843a0b1cea
Mitterauer Verena EXT <mitterv@hpc-login04.iter.org>
2019-08-12 10:11:24 +0200

    putNonTimed() only for first timestep

--- workflow/hcd_wrapper.py
+++ workflow/hcd_wrapper.py
@@ -136,7 +136,8 @@ def hcd_wrapper(par_path):
       for elem in ids_bundle_work:    # not sure if this should be work or updated
 
         ids_bundle_work[elem].setExpIdx(idx_out)
-        ids_bundle_work[elem].putNonTimed()
+        if timenow ==  param['tbegin']+= param['dt_required']:
+          ids_bundle_work[elem].putNonTimed()
         ids_bundle_work[elem].putSlice()
 
       
