d0ccd6d37a46fec3398119de0a83169b4a0b761e
Mireille Schneider <Mireille.Schneider@iter.org>
2019-10-25 09:51:58 +0200

    Modules unified to be intel by default; configuration for new LL added

--- config_hpc.sh
+++ config_hpc.sh
@@ -1,25 +1,18 @@
 module purge
 
 # IMAS, Kepler, FC2K
-module load IMAS/3.21.0-3.8.11
-module load Kepler/2.5p4-2.1.5
-module load FC2K/4.4.0
+module load IMAS
+module load Kepler
+module load FC2K
 
 # Need to remove the stack limit to avoid segmentation fault inside codes
 ulimit -Ss unlimited
 
-# Switch from Python 2.7 to Python 3
-source switch_python_ITER_HPC.sh
-
 # For Python actors
-module load PyUAL/1.0.0-intel-2018a-Python-3.6.4
+module load PyUAL
 
 # For actor release procedure
 module load sh/1.12.14-intel-2018a-Python-3.6.4
-
-# Compile actors without diagnostic information
-# (the diag info is not compatible with Python for this old version of FC2K)
-export DIAG_INFO=-DNO_DIAG_INFO
 
 # Library to process xml with Python
 module load lxml/4.2.0-intel-2018a-Python-3.6.4
@@ -28,11 +21,12 @@ module load lxml/4.2.0-intel-2018a-Python-3.6.4
 module load FRUIT/3.4.3-intel-2018a-Ruby-2.5.1
 module load FRUIT_processor/3.4.3-intel-2018a-Ruby-2.5.1
 module load interpos/8.2.1-ifort
-module load XMLlib/3.1.0-intel-2018a
+module load XMLlib/3.2.0-intel-2018a
 module load PSPLINE/20181008-intel-2018a
+module load PyUAL/1.0.2-intel-2018a-Python-3.6.4
 
 # To read Machine Description data from the MD datbase
-export MD_ACCESS=yes
+export MD_ACCESS=no
 if [ $MD_ACCESS = "yes" ]; then
     echo "m-machine-description module loaded"
     module use --append m-machine-description/src/main/Environment/HPC/modules
