5c3d1b117f4894cc5ae23c1ad9cf32ca26494b52
Mireille Schneider <Mireille.Schneider@iter.org>
2019-11-22 17:15:53 +0100

    Remove m-machine-description from the general configuration since it belonged to the waveform editor which has been moved to another GIT repository

--- config_hpc.sh
+++ config_hpc.sh
@@ -26,16 +26,3 @@ module load XMLlib/3.2.0-intel-2018a
 module load PSPLINE/20181008-intel-2018a
 module load PyAL/1.1.1-intel-2018a-Python-3.6.4
 module load NAG/26-intel-2018a
-
-# To read Machine Description data from the MD database
-export MD_ACCESS=yes
-if [ $MD_ACCESS = "yes" ]; then
-    #echo "m-machine-description module loaded"
-    module use --append m-machine-description/src/main/Environment/HPC/modules
-    module load m-machine-description
-fi
-
-#else
-    #echo "Warning: m-machine-description module not loaded"
-#fi
-
