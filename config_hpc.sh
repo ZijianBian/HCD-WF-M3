b54b08c213eeaa3ae438e78bd3b8861d529ed67e
Mireille Schneider <Mireille.Schneider@iter.org>
2020-02-10 13:19:12 +0100

    Update PyAL version

--- config_hpc.sh
+++ config_hpc.sh
@@ -23,10 +23,11 @@ module load FRUIT_processor/3.4.3-intel-2018a-Ruby-2.5.1
 module load interpos/8.2.1-ifort
 module load XMLlib/3.2.0-intel-2018a
 module load PSPLINE/20181008-intel-2018a
-module load PyAL/1.1.1-intel-2018a-Python-3.6.4
+module load PyAL/1.1.2-intel-2018a-Python-3.6.4
 module load NAG/26-intel-2018a
 
 # Location for IMAS actors (pre-compiled)
 export ACTOR_POOL=$PWD/actor_install/actors
+export KEPLER_OLD=$KEPLER
 export KEPLER=$ACTOR_POOL # (still needed by IMAS actors themselves)
 
