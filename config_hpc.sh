b50ead831ae8fc3d069630e44df6243b9a96ea3a
Mireille Schneider <Mireille.Schneider@iter.org>
2020-02-25 17:43:07 +0100

    Cleanly manage HCD actors to deal with when a local kepler is loaded or not

--- config_hpc.sh
+++ config_hpc.sh
@@ -1,9 +1,30 @@
+
+# Location of IMAS actors (pre-compiled)
+if [ -z "$KEPLER_DOT" ]
+then
+    export ACTOR_POOL=$PWD/actor_install/actors
+    export KEPLER_OLD=$KEPLER
+    export KEPLER=$ACTOR_POOL # (still needed by IMAS actors themselves)
+    unset local_kepler
+    echo "H&CD actors taken from ~/actor_install/actors"
+else
+    export local_kepler=`echo $KEPLER_DOT | awk -F "/" '{print $NF}'`
+    export ACTOR_POOL=$KEPLER_DOT/kepler/
+    echo "Local Kepler" $local_kepler "loaded --> H&CD actors taken from there"
+fi
+
 module purge
 
 # IMAS, Kepler, FC2K
 module load IMAS
 module load Kepler
 module load FC2K
+
+# Re-load Kepler if it was loaded already
+if [ "$local_kepler" ]
+then
+    kepler_load $local_kepler >& /dev/null
+fi
 
 # Need to remove the stack limit to avoid segmentation fault inside codes
 ulimit -Ss unlimited
@@ -25,9 +46,4 @@ module load XMLlib/3.2.0-intel-2018a
 module load PSPLINE/20181008-intel-2018a
 module load PyAL/1.1.2-intel-2018a-Python-3.6.4
 module load NAG/26-intel-2018a
-
-# Location for IMAS actors (pre-compiled)
-export ACTOR_POOL=$PWD/actor_install/actors
-export KEPLER_OLD=$KEPLER
-export KEPLER=$ACTOR_POOL # (still needed by IMAS actors themselves)
 
