a69ed825f8a273d161698e5a87d80c536fbd6552
Mireille Schneider <Mireille.Schneider@iter.org>
2020-04-21 20:15:29 +0200

    Add comments and clean create_maindict.py

--- interface/create_maindict.py
+++ interface/create_maindict.py
@@ -1,52 +1,59 @@
+#---------------------------------------------------------------------------------
+# CREATE A PYTHON DICTIONARY (MAINDICT) THAT CONTAINS THE NAME OF ALL H&CD CODES,
+# THEIR INPUT & OUTPUT IDSS, THEIR CATEGORY (EC_WAVESOLVER, NBI_SOURCE, ..), 
+# AND THE H&CD SYSTEM THEY BELONG TO (EC, IC, NBI, ALPHA)
+#---------------------------------------------------------------------------------
 import os, imas, sys
-sys.path.append('interface')
-sys.path.append('workflow')
-sys.path.append(os.getcwd())
 from lxml import etree
-from hcd_tools import import_actor, loadlist
+from hcd_tools import import_actor
 
 def create_maindict(default_workflow_parameters):
-    #---------------------------------------------------------------------------------------------
-    # CREATE A PYTHON DIRECTORY (MAINDICT) THAT CONTAINS THE NAME OF ALL CODES (NEMO, BBNBI, ...) , 
-    # THEIR INPUT & OUTPUT IDSS, THEIR CATEGORY (EC_WAVESOLVER, NBI_SOURCE, ..), 
-    # THE HEATING SYSTEM THEY BELONG TO (EC, IC, NBI, ALPHA)
-    #---------------------------------------------------------------------------------------------
-
-    ids_list = loadlist('ids_list')
-
-    not_compiled_list = []
 
     def read_inputoutput(name):
-        in_l  = []
-        out_l = []
+        from hcd_tools import loadlist
+        ids_list = loadlist('ids_list')
+        input_ids_list  = []
+        output_ids_list = []
         err = import_actor(name)
         if err == 0:
             parstr = globals()[name].__doc__
             for iids in ids_list:
                 if parstr.find(':param '+iids) is not -1:
-                    in_l.append(iids)
+                    input_ids_list.append(iids)
                 if parstr.find(':param result: '+iids) is not -1:
-                    out_l.append(iids)
-        else:
-            not_compiled_list.append(name)
-        return(in_l, out_l)
+                    output_ids_list.append(iids)
+        return(input_ids_list,output_ids_list,err)
 
+    # READ THE ACTOR_SELECTION STRUCTURE OF PARAMETERS FROM THE WORKFLOW INPUT XML FILE
     tree = etree.parse(default_workflow_parameters)
     root = tree.getroot()
+    actor_selection = root[2]
 
+    # -------------------------------
+    # MAINDICT CONTAINS 2 MAIN KEYS:
+    # - SYSTEMS      ---> 4 SYSTEMS: ECRH, ICRH, NBI, NUCLEAR              ---> CATEGORIES: EC_WAVE_SOLVER, ETC.
+    # - POST_PROCESS ---> 2 SYSTEMS: FILL_CORE_SOURCES, FILL_CORE_PROFILES ---> CATEGORIES: SOURCE, PROFILES
+    # -------------------------------
+    # INSIDE EACH CATEGORY (EC_WAVE_SOLVER, IC_WAVE_SOLVER, IC_WAVE_FP, ...):
+    # - KEYS ARE ACTOR NAMES
+    # - VALUES ARE INPUT/OUTPUT IDSS
+    # -------------------------------
+    not_compiled_list = []
     maindict = {}
-    for step in root[2]:
-        dict3 = {}
-        for isys in step:
-            dict2 = {}
-            for icat in isys:
-                dict1 = {}
-                if icat.tag is not etree.Comment:
-                    for icode in icat.attrib['list'].split():
-                        (in_l, out_l) = read_inputoutput(icode)
-                        dict1[icode] = [in_l, out_l]
-                    dict2[icat.tag] = dict1
-            dict3[isys.tag] = dict2
-        maindict[step.tag] = dict3
+    for sub_structure in actor_selection:
+        dict_system = {}
+        for system in sub_structure:
+            dict_category = {}
+            for category in system:
+                dict_actor = {}
+                if category.tag is not etree.Comment:
+                    for actor_name in category.attrib['list'].split():
+                        (input_ids_list, output_ids_list, err) = read_inputoutput(actor_name)
+                        if err != 0:
+                            not_compiled_list.append(actor_name)
+                        dict_actor[actor_name] = [input_ids_list, output_ids_list]
+                    dict_category[category.tag] = dict_actor
+            dict_system[system.tag] = dict_category
+        maindict[sub_structure.tag] = dict_system
 
     return(maindict,not_compiled_list)
