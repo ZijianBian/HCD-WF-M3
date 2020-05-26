815cda7b81a39021d895d97d17880872744919ac
Mireille Schneider <Mireille.Schneider@iter.org>
2020-05-26 09:52:44 +0200

    Modified generate_actors to take an absolute path for the default input xml file

--- workflow/generate_actors.py
+++ workflow/generate_actors.py
@@ -3,11 +3,12 @@
 #          ACCORDING TO THE ACTOR SELECTION FROM THE GUI
 # -------------------------------------------------------
 from hcd_tools import import_actor, loadlist, create_maindict, is_compiled_for_mpi
+from os import getenv
 
 # CREATE THE DICTIONARY CONTAINING THE INFORMATION OF ALL CHOSEN ACTORS
 # (SYSTEM, CATEGORY, ACTOR NAME, INPUT/OUTPUT IDSS)
 (maindict, compiled_actors, uncompiled_actors, code_selection) = \
-    create_maindict('input_workflow_default.xml',2,0)
+    create_maindict(getenv('HCD_FOLDER')+'/global_configuration/input_workflow_default.xml',2,0)
 
 # LIST OF EMPTY ACTORS AND OF EXTRA (NON-IDS) ARGUMENTS FOR EACH ACTOR
 empty_actor_list    = loadlist('empty_actor_list')
@@ -18,8 +19,8 @@ extra_argument_list = loadlist('extra_arguments')
 with open('workflow/auto_hcd_actors.py', 'w') as file:
 
     file.write('from hcd_tools import import_actor, loadlist\n\n')
-    file.write('list_of_actors = ["'+'","'.join(compiled_actors+empty_actor_list+merge_actor_list)\
-               +'"]\n\n')
+    file.write('list_of_actors = ["'+'","'.join(compiled_actors\
+               +empty_actor_list+merge_actor_list)+'"]\n\n')
     file.write('for name in list_of_actors:\n')
     file.write('   err = import_actor(name,0)\n\n')
 
@@ -29,7 +30,7 @@ with open('workflow/auto_hcd_actors.py', 'w') as file:
 
                 file.write('def '+ cat + '(bundle, parameters): \n')
                 i = 0
-                
+
                 for code in maindict[proc][sys][cat]:
                     err = import_actor(code,0)
                     extra_arg_nr = 0
@@ -47,11 +48,11 @@ with open('workflow/auto_hcd_actors.py', 'w') as file:
                         for ids_in in maindict[proc][sys][cat][code][0]:
                             if first_in == False:
                                 file.write(', ')
-                            
+
                             if ids_in.find('extra_argument_list') is not -1\
                                and extra_argument_list.get(code) is not None:
-                                file.write('parameters["'+extra_argument_list[code][extra_arg_nr]\
-                                           +'"]')
+                                file.write('parameters["'\
+                                +extra_argument_list[code][extra_arg_nr]+'"]')
                                 extra_arg_nr += 1
                             elif ids_in.find('codeparam') is not -1:
                                 file.write('(parameters["input_path"]+"/'\
@@ -60,18 +61,21 @@ with open('workflow/auto_hcd_actors.py', 'w') as file:
                                 file.write('bundle["'+ids_in+'"]')
                             first_in = False
 
-                        libmpi_path = eval(code+'.location')+'/native_wrapper/lib/lib'+code+'.so'
+                        libmpi_path = eval(code+'.location')+'/native_wrapper/lib/lib'\
+                                      +code+'.so'
                         if is_compiled_for_mpi(libmpi_path, 'libmpi'):
                             if cat == 'nbi_fp':
-                                file.write(',"mpi_local",mpi_processes=parameters["nproc_ion_fp"]')
+                                file.write\
+                                    (',"mpi_local",mpi_processes=parameters["nproc_ion_fp"]')
                             else:
-                                file.write(',"mpi_local"') # FOR NON-FP CODES, DEFAULT IS NPROC=4
+                                file.write\
+                                    (',"mpi_local"') # FOR NON-FP CODES, DEFAULT IS NPROC=4
 
                         file.write(')\n')
                     else:
                         file.write('       print("Code not installed")\n\n')
-                        output_ids_list = ['core_profiles'] # DEFAULT WHEN NO CODE FOR THIS SOURCE
-                
+                        output_ids_list = ['core_profiles'] # DEFAULT
+
                 file.write('   else: \n')
                 file.write('       '+output_ids_list[0]+'_temp = empty_'\
                            +output_ids_list[0]+ '(bundle["core_profiles"])')
