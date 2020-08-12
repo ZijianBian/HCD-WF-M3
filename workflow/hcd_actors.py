8e483983409fb5a341fe0a88d9bb41cade95f91c
Olivier Hoenen <olivier.hoenen@ipp.mpg.de>
2020-08-12 10:04:33 +0200

    Taking non IDS arguments into account in actor runner proxy

--- workflow/hcd_actors.py
+++ workflow/hcd_actors.py
@@ -15,36 +15,30 @@ empty_actor_list    = loadlist('empty_actor_list')
 merge_actor_list    = loadlist('merge_actor_list')
 extra_argument_list = loadlist('extra_arguments')
 
-#print(maindict)
-#print(compiled_actors)
-#print(empty_actor_list)
-#print(merge_actor_list)
-#print(extra_argument_list)
-
 list_of_actors = compiled_actors+empty_actor_list+merge_actor_list
 for name in list_of_actors:
     err = import_actor(name,0)
 
-#print(list_of_actors)
-
 def run(cat, bundle, parameters):
     # Get list of all codes in that category
     catlist = next(gen_dict_extract(cat,maindict))
-    #print(catlist)
     codeinfo = catlist[parameters[cat]]
-    #print(codeinfo['name'])
-    #print(codeinfo['input'])
-    #print(codeinfo['output'])
-    inputids = [bundle[ids] for ids in codeinfo['input']]
+    code = codeinfo['name']
+    inputids = []
+    inputextra = []
+    extra_arg_nr = 0
+    inputxml = []
+    for i in codeinfo['input']:
+        if i.find('extra_argument_list') is not -1 \
+           and extra_argument_list.get(code) is not None:
+            inputextra.append(parameters[extra_argument_list[code][extra_arg_nr]])
+            extra_arg_nr += 1
+        elif i.find('codeparam') is not -1:
+            inputxml.append(parameters['input_path']+'/'+ \
+                              codeinfo['system']+'/input_'+code+'.xml')
+        else:
+            inputids.append(bundle[i])
+    inputs = inputids + inputextra + inputxml
     # Call of the chosen code
-    return globals()[codeinfo['name']](*inputids)
+    return globals()[code](*inputs)
 
-#parameters = create_workflow_param_from_file(workflow_xml,2)
-
-#print(parameters)
-#catlist = next(gen_dict_extract('ec_wave_solver',maindict))
-#print(catlist)
-#print(parameters['ec_wave_solver'])
-#print(catlist[parameters['ec_wave_solver']])
-#bundle = None
-#run_actors('ec_wave_solver',None,parameters)
