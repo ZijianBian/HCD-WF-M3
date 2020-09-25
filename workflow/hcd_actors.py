a1c4d3a2d4fd6f223efb0a9638281b5ab748ecfd
Olivier Hoenen <Olivier.Hoenen@iter.org>
2020-09-25 14:42:15 +0200

    Should not separate IDS and extra input arguments as order matters

--- workflow/hcd_actors.py
+++ workflow/hcd_actors.py
@@ -24,20 +24,19 @@ def run(cat, bundle, parameters):
     codeslist = catdict[cat] #next(gen_dict_extract(cat,maindict))
     codeinfo = codeslist[parameters[cat]]
     code = codeinfo['name']
-    inputids = []
-    inputextra = []
+    inputargs = []
     extra_arg_nr = 0
     inputxml = []
     for i in codeinfo['input']:
         if i.find('extra_argument_list') is not -1 \
            and extra_argument_list.get(code) is not None:
-            inputextra.append(parameters[extra_argument_list[code][extra_arg_nr]])
+            inputargs.append(parameters[extra_argument_list[code][extra_arg_nr]])
             extra_arg_nr += 1
         elif i.find('codeparam') is not -1:
             inputxml.append(parameters['input_path']+'/'+ \
                               codeinfo['system']+'/input_'+code+'.xml')
         else:
-            inputids.append(bundle[i])
+            inputargs.append(bundle[i])
 
 
     inputmpi = []
@@ -49,7 +48,7 @@ def run(cat, bundle, parameters):
         else:
             inputmpi.append(['mpi_local']) # FOR NON-FP CODES, DEFAULT IS NPROC=4
 
-    inputs = inputids + inputextra + inputxml + inputmpi
+    inputs = inputargs + inputxml + inputmpi
     # Call of the chosen code
     return globals()[code](*inputs)
 
