b8431035b27c55be386f6c421b848bb415dacfff
Olivier Hoenen <Olivier.Hoenen@iter.org>
2022-02-07 16:58:54 +0100

    Remove mentions to CentOS7 modules

--- envs/iter/10_setenv_imas_monorepo.sh
+++ envs/iter/10_setenv_imas_monorepo.sh
@@ -3,8 +3,7 @@
 set -e
 
 # Use the parts of IMAS
-module load Python/3.6.4-intel-2018a
-module load Cython/0.28.6-intel-2018a-Python-3.6.4
+# module load Python/3.8.6-GCCcore-10.2.0
 
 # But for now import IMAS itself
 # TODO: Move to split repo
