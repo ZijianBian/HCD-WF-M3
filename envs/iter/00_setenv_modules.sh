b743cb19a76c908085c1e2461ba078439664a8b7
Simon Pinches <Simon.Pinches@iter.org>
2022-05-01 13:54:03 +0200

    Remove superfluous modulepath and use Python from IMAS module

--- envs/iter/00_setenv_modules.sh
+++ envs/iter/00_setenv_modules.sh
@@ -4,6 +4,5 @@ set -e
 
 # Set up environment
 . /usr/share/Modules/init/sh
-module use /work/imas/etc/modulefiles
 module use /work/imas/etc/modules/all
 module purge
--- envs/iter/10_setenv_imas_monorepo.sh
+++ envs/iter/10_setenv_imas_monorepo.sh
@@ -2,9 +2,5 @@
 # Set up user-like IMAS env
 set -e
 
-# Use the parts of IMAS
-# module load Python/3.8.6-GCCcore-10.2.0
-
-# But for now import IMAS itself
 # TODO: Move to split repo
 module load IMAS
