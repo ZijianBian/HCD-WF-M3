e832c4ce8a5d29f91695bce9ba7bb96fdaf26f44
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2023-09-20 17:03:45 +0200

    modified function to run hcd workflow on single time slice..

--- src/dbhelper.py
+++ src/dbhelper.py
@@ -2,8 +2,18 @@ import imas
 import os
 import sys
 
+
 class DbHelper:
-    def __init__(self, input_user_or_path, input_database, output_user_or_path, output_database, shot_number, input_run, output_run):
+    def __init__(
+        self,
+        input_user_or_path,
+        input_database,
+        output_user_or_path,
+        output_database,
+        shot_number,
+        input_run,
+        output_run,
+    ):
         # DEFAULT OUTPUT USER_OR_PATH IS $USER
         if output_user_or_path == "default":
             output_user_or_path = os.getenv("USER")
@@ -25,15 +35,15 @@ class DbHelper:
                 file=sys.stdout,
             )
             os.makedirs(output_folder)
-        
+
         self.input_user_or_path = input_user_or_path
-        self.input_database =input_database
-        
-        self.output_user_or_path =output_user_or_path
-        self.output_database =output_database
-        self.shot_number =shot_number
-        self.input_run =input_run
-        self.output_run =output_run
+        self.input_database = input_database
+
+        self.output_user_or_path = output_user_or_path
+        self.output_database = output_database
+        self.shot_number = shot_number
+        self.input_run = input_run
+        self.output_run = output_run
 
     def getInputDatabase(self):
         # OPEN INPUT DATAFILE
@@ -60,7 +70,7 @@ class DbHelper:
             )
             print("   Please check that the file exists.", file=sys.stderr)
         return inputDb
-    
+
     def getOutputDatabase(self):
         # CREATE OUTPUT DATAFILE
         outputDb = imas.DBEntry(
@@ -85,8 +95,26 @@ class DbHelper:
             )
             print("   --> Aborted.", file=sys.stderr)
         return outputDb
-        
+
     def getMachineDatabase(self):
-        machineDb = imas.DBEntry(imas.imasdef.MEMORY_BACKEND,self.output_database,0,self.output_run,self.output_user_or_path)
+        machineDb = imas.DBEntry(
+            imas.imasdef.MEMORY_BACKEND,
+            self.output_database,
+            0,
+            self.output_run,
+            self.output_user_or_path,
+        )
         machineDb.create()
         return machineDb
+
+    def getTimeArray(self, inputDb):
+        time_array = None
+        try:
+            time_array = inputDb.partial_get(ids_name="equilibrium", data_path="time")
+        except Exception:
+            print(
+                "  ERROR while reading the core_profiles IDS: is it really present in the input file?",
+                file=sys.stderr,
+            )
+            print("  ----> Aborted.", file=sys.stderr)
+        return time_array
