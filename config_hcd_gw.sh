8456cf728a51bbf7b8c76b4767a23246a8325004
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2023-12-14 11:20:07 +0100

    removed local development config files to not to confuse with multiple configuration files

--- config_hcd_gw.sh
+++
@@ -1,5 +0,0 @@
-module load imasenv/3.38.1/intel/2020/2.0
-module load itm-python/3.10
-export ACTOR_FOLDER=/afs/eufus.eu/user/g/g2ncummi/work/hcd-python-actors/3.38.1
-export PYTHONPATH=$ACTOR_FOLDER:$PYTHONPATH
-module load iwrap
