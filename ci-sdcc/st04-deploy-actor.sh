b3de0ac853f05865fb27e51f82f09b616781234a
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2024-05-23 10:03:20 +0200

    disable eb check style

--- ci-sdcc/st04-deploy-actor.sh
+++ ci-sdcc/st04-deploy-actor.sh
@@ -79,7 +79,7 @@ sed -e "s;__COMMITHASH__;${COMMITHASH};" \
     ./ci-sdcc/ebfiles/"$MODULE_NAME".eb.in >./ci-sdcc/ebfiles/"$MODULE_FULL_VERSION"
 
 #format eb file
-python3 -m venv build_venv && source build_venv/bin/activate && pip install --upgrade pip && pip install black && black ./ci-sdcc/ebfiles/"$MODULE_FULL_VERSION" && deactivate
+python3 -m venv build_venv && source build_venv/bin/activate && pip install --upgrade pip && pip install black && black --line-length 80 ./ci-sdcc/ebfiles/"$MODULE_FULL_VERSION" && deactivate
 rm -rf build_venv
 # create eb tar file
 tar -cvzf eb.tar.gz ./ci-sdcc/ebfiles/"$MODULE_FULL_VERSION" >/dev/null 2>&1
@@ -136,7 +136,7 @@ module use -p /work/imas/opt/bamboo_deploy/easybuild/modules/all
 eb ./ci-sdcc/ebfiles/"$MODULE_FULL_VERSION" --inject-checksums ${EB_OPTS//\'/} "$EB_HTTP_OPTS"
 
 # check style
-eb ./ci-sdcc/ebfiles/"$MODULE_FULL_VERSION" --check-style ${EB_OPTS//\'/} "$EB_HTTP_OPTS"
+# eb ./ci-sdcc/ebfiles/"$MODULE_FULL_VERSION" --check-style ${EB_OPTS//\'/} "$EB_HTTP_OPTS"
 
 # # execute eb command
 eb ./ci-sdcc/ebfiles/"$MODULE_FULL_VERSION" ${EB_OPTS//\'/} "$EB_HTTP_OPTS"
