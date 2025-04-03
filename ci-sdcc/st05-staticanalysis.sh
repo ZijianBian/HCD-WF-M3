a954cb2be43b2062b573bb1c802b47395d4bdd12
sawantp1 <prasadparshuram.sawantdesai@iter.org>
2025-04-03 12:08:59 +0200

    created logs folders

--- ci-sdcc/st05-staticanalysis.sh
+++ ci-sdcc/st05-staticanalysis.sh
@@ -15,33 +15,36 @@ python -m venv "$ENVIRONEMNT_NAME"
 . "$ENVIRONEMNT_NAME"/bin/activate
 # Install and run linters
 pip install --upgrade 'black >=24,<25' flake8 pylint ruff
-
+mkdir -p black_logs
+mkdir -p flake8_logs
+mkdir -p pylint_logs
+mkdir -p ruff_logs
 echo "---------------------------------------------------------------------"
 echo "executing black"
-black --check -l 120 hcdworkflow | tee black_hcdworkflow.log
-black --check -l 120 tools | tee black_tools.log
-black --check -l 120 gui | tee black_gui.log
-black --check -l 120 workflow | tee black_workflow.log
+black --check -l 120 hcdworkflow | tee black_logs/hcdworkflow.log
+black --check -l 120 tools | tee black_logs/tools.log
+black --check -l 120 gui | tee black_logs/gui.log
+black --check -l 120 workflow | tee black_logs/workflow.log
 echo "---------------------------------------------------------------------"
 echo "executing flake8"
-flake8 --max-line-length=120 --ignore=E203,W503 hcdworkflow | tee flake8_hcdworkflow.log
-flake8 --max-line-length=120 --ignore=E203,W503 tools | tee flake8_tools.log
-flake8 --max-line-length=120 --ignore=E203,W503 workflow | tee flake8_workflow.log
-flake8 --max-line-length=120 --ignore=E203,W503 gui | tee flake8_gui.log
+flake8 --max-line-length=120 --ignore=E203,W503 hcdworkflow | tee flake8_logs/hcdworkflow.log
+flake8 --max-line-length=120 --ignore=E203,W503 tools | tee flake8_logs/tools.log
+flake8 --max-line-length=120 --ignore=E203,W503 workflow | tee flake8_logs/workflow.log
+flake8 --max-line-length=120 --ignore=E203,W503 gui | tee flake8_logs/gui.log
 
 echo "---------------------------------------------------------------------"
 echo "executing pylint"
-pylint --max-line-length=120 --disable=E0401 -E --ignore=_version.py ./hcdworkflow/*.py | tee pylint_hcdworkflow.log 
-pylint --max-line-length=120 --disable=E0401 -E --ignore=_version.py ./tools/*.py | tee pylint_tools.log
-pylint --max-line-length=120 --disable=E0401 -E --ignore=_version.py ./gui/*.py | tee pylint_gui.log
-pylint --max-line-length=120 --disable=E0401 -E --ignore=_version.py ./workflow/*.py | tee pylint_workflow.log
+pylint --max-line-length=120 --disable=E0401 -E --ignore=_version.py ./hcdworkflow/*.py | tee pylint_logs/hcdworkflow.log 
+pylint --max-line-length=120 --disable=E0401 -E --ignore=_version.py ./tools/*.py | tee pylint_logs/tools.log
+pylint --max-line-length=120 --disable=E0401 -E --ignore=_version.py ./gui/*.py | tee pylint_logs/gui.log
+pylint --max-line-length=120 --disable=E0401 -E --ignore=_version.py ./workflow/*.py | tee pylint_logs/workflow.log
 echo "---------------------------------------------------------------------"
 
 echo "executing ruff"
-ruff check hcdworkflow --select F401,E402 | tee ruff_hcdworkflow.log
-ruff check tools --select F401,E402  | tee ruff_tools.log
-ruff check gui --select F401,E402  | tee ruff_gui.log
-ruff check workflow --select F401,E402  | tee ruff_workflow.log
+ruff check hcdworkflow --select F401,E402 | tee ruff_logs/hcdworkflow.log
+ruff check tools --select F401,E402  | tee ruff_logs/tools.log
+ruff check gui --select F401,E402  | tee ruff_logs/gui.log
+ruff check workflow --select F401,E402  | tee ruff_logs/workflow.log
 
 deactivate
 rm -rf "$ENVIRONEMNT_NAME"
